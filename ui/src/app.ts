3c2507462eaeca96223b837cd090f9b68371a079
Alex Davydov <alex-d@live.co.uk>
2025-07-28 10:09:36 +0200

    fix app.ts

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,15 +1,8 @@
-import { convertMetricsUI, model } from '@platforma-open/milaboratories.repertoire-distance-2.model';
+import { model } from '@platforma-open/milaboratories.repertoire-distance-2.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import DistanceGraph from './pages/DistanceGraph.vue';
-import MainPage from './pages/MainPage.vue';
 
-export const sdkPlugin = defineApp(model, (app) => {
-  watch(
-    () => app.model.ui.metrics,
-    debounce((metrics) => convertMetricsUI(toRaw(metrics)), 500),
-    { deep: true, immediate: true },
-  );
-
+export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
       '/': () => DistanceGraph,
